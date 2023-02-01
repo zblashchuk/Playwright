15b443b0bca9718100c818d7dcc6d05356959620
Zoryana Blashchuk <zblashchuk@gmail.com>
2023-02-01 23:53:41 +0200

    4

--- playwright-devices.config.ts
+++ playwright-devices.config.ts
@@ -35,7 +35,7 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    headless: false,
+    headless: true,
     
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
