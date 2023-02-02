7c755138465817536122b5387fed451fe1a154f2
Zoryana Blashchuk <zblashchuk@gmail.com>
2023-02-02 16:08:33 +0200

    7

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,6 +33,7 @@ const config: PlaywrightTestConfig = {
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["html", { open: "never" }],
+    ["line"],
     ['allure-playwright'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
